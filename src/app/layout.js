9f7c058a742f732f7a8fae84b3159216b8210693
Iftekhar Jahan Soron <67633243+Soron248@users.noreply.github.com>
2024-05-05 16:04:09 +0600

    lil change

--- src/app/layout.js
+++ src/app/layout.js
@@ -13,7 +13,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <div className="p-5">
+        <div className="px-5">
           <Navbar />
           {children}
         </div>
