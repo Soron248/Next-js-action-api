ba7b1918a3a38b9201006dc79b93434bbf365d35
Iftekhar Jahan Soron <67633243+Soron248@users.noreply.github.com>
2024-05-06 19:59:08 +0600

    maping fix

--- src/components/TopicsCard.jsx
+++ src/components/TopicsCard.jsx
@@ -18,27 +18,28 @@ const getAllTopics = async () => {
 };
 
 const TopicsCard = async () => {
-  const { topics } = await getAllTopics();
+  const topicRes = await getAllTopics();
   return (
     <>
-      {topics.reverse().map((t) => (
-        <div
-          className="max-w-5xl mx-auto shadow-md p-5 rounded-md flex mb-5"
-          key={t._id}
-        >
-          <div className="w-5/6">
-            <h1 className="text-teal-800 font-bold text-2xl">{t.title}</h1>
-            <p className="text-gray-400 text-lg">{t.description}</p>
-          </div>
+      {topicRes &&
+        topicRes.topics.reverse().map((t) => (
+          <div
+            className="max-w-5xl mx-auto shadow-md p-5 rounded-md flex mb-5"
+            key={t._id}
+          >
+            <div className="w-5/6">
+              <h1 className="text-teal-800 font-bold text-2xl">{t.title}</h1>
+              <p className="text-gray-400 text-lg">{t.description}</p>
+            </div>
 
-          <div className="w-1/6 flex justify-end items-start gap-5">
-            <Remove id={t._id} />
-            <Link href={`update_topic/${t._id}`}>
-              <MdEdit size={25} className="text-blue-600" />
-            </Link>
+            <div className="w-1/6 flex justify-end items-start gap-5">
+              <Remove id={t._id} />
+              <Link href={`update_topic/${t._id}`}>
+                <MdEdit size={25} className="text-blue-600" />
+              </Link>
+            </div>
           </div>
-        </div>
-      ))}
+        ))}
     </>
   );
 };
